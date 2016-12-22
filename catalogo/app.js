4779f34d380b5780929835640b0b14364f0ae2d2
Marcio Barros <marcio.barros@gmail.com>
2016-12-22 07:00:41 -0200

    catalogo

--- catalogo/app.js
+++
@@ -1,9 +0,0 @@
-var CodeSnackApp = angular.module('CodeSnackApp', []);
-
-CodeSnackApp.controller('TodoCtrl', function($scope, $http) {
-
-	$http.get('catalogo.json')
-		.then(function(res){
-        	$scope.dados = res.data;
-        });
-});
